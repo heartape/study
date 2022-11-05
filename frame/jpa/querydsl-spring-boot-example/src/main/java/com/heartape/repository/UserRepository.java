9d767fc043476ad76050098ba3eb9f763fb3fad1
heartape <64122416+heartape@users.noreply.github.com>
2022-11-05 23:55:58 +0800

    querydsl-spring-boot-example

--- frame/jpa/querydsl-spring-boot-example/src/main/java/com/heartape/repository/UserRepository.java
+++ frame/jpa/querydsl-spring-boot-example/src/main/java/com/heartape/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.heartape.repository;
 
 import com.heartape.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, Integer>, JpaSpecificationExecutor<User>, QuerydslPredicateExecutor<User> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>, QuerydslPredicateExecutor<User> {
 }
--- frame/jpa/querydsl-spring-boot-example/src/main/java/com/heartape/service/impl/UserServiceImpl.java
+++ frame/jpa/querydsl-spring-boot-example/src/main/java/com/heartape/service/impl/UserServiceImpl.java
@@ -4,9 +4,12 @@ import com.heartape.entity.Phone;
 import com.heartape.entity.QPhone;
 import com.heartape.entity.QUser;
 import com.heartape.entity.User;
+import com.heartape.repository.UserRepository;
 import com.heartape.service.IUserService;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,15 +18,24 @@ public class UserServiceImpl implements IUserService {
     @Autowired
     private JPAQueryFactory queryFactory;
 
+    @Autowired
+    private UserRepository userRepository;
+
     /**
-     * 单表查询
+     * 单表查询，必要时与hibernate结合使用
      * <li>使用QUser和QPhone类之前，需要先进行compile
      * <li>然后根据maven apt-maven-plugin插件的outputDirectory,将target/generated-sources/java文件夹“mark directory as -> generated sources root”
      * <li>最后直接在项目中使用QUser和QPhone类，而不用将这两个类拷入项目
      */
     @Override
     public User getOne(Integer id) {
+        // hibernate crud接口
+        User user1 = userRepository.findById(id).orElseThrow(RuntimeException::new);
+        Page<User> userPage = userRepository.findAll(PageRequest.of(1, 10));
+
         QUser user = QUser.user;
+        // Querydsl crud接口
+        User user2 = userRepository.findOne(user.id.eq(id)).orElseThrow(RuntimeException::new);
         return queryFactory
                 .select(user)
                 .from(user)
