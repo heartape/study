8d6b808f5df4a72e7a7d6866c42e1ce9fce5d34a
heartape <64122416+heartape@users.noreply.github.com>
2023-04-19 09:52:17 +0800

    flink-mysql-to-es

--- frame/flink/flink-mysql-to-es/src/main/java/com/heartape/FlinkMysqlApplication.java
+++ frame/flink/flink-mysql-to-es/src/main/java/com/heartape/FlinkMysqlApplication.java
@@ -3,30 +3,51 @@ package com.heartape;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.google.gson.reflect.TypeToken;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.table.StartupOptions;
 import com.ververica.cdc.debezium.JsonDebeziumDeserializationSchema;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.elasticsearch.sink.Elasticsearch7SinkBuilder;
 import org.apache.flink.connector.elasticsearch.sink.ElasticsearchSink;
 import org.apache.flink.connector.elasticsearch.sink.FlushBackoffType;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.Requests;
 
-import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.Properties;
 
+@Slf4j
 public class FlinkMysqlApplication {
 
-    public final static String MYSQL_SERVER = "192.168.30.100";
-    public final static String ES_SERVER = "192.168.30.100";
+    public final static String MYSQL_SERVER = "192.168.31.100";
+    public final static String ES_SERVER = "192.168.31.100";
 
     public static void main(String[] args) {
+
+        // 保存checkpoint的位置
+        // String checkpointStorage = "file:///C:\\Users\\heartape\\Desktop";
+        // String checkpointStorage = "file:///opt/flink/checkpoints/es";
+        String checkpointStorage = args[0];
+
+        // 读取checkpoint的位置，可以在命令行直接注入，不需要在java代码中操作
+        // String source = "/af3d2ad769ad3b3a18ec768427ed66df/chk-6";
+        // Configuration configuration = Configuration.fromMap(Map.of("execution.savepoint.path", checkpointStorage + source));
+        // StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment(configuration);
+
+        Properties properties = new Properties();
+        properties.setProperty("bigint.unsigned.handling.mode","long");
+        properties.setProperty("decimal.handling.mode","double");
+        properties.setProperty("dateConverters.format.date", "yyyy-MM-dd");
+        properties.setProperty("dateConverters.format.timestamp.zone", "UTC+8");
 
         try (StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment()) {
             MySqlSource<String> mySqlSource = MySqlSource.<String>builder()
@@ -36,6 +57,7 @@ public class FlinkMysqlApplication {
                     .tableList("flink.book") // set captured table
                     .username("root")
                     .password("root")
+                    .debeziumProperties(properties)
                     .deserializer(new JsonDebeziumDeserializationSchema()) // converts SourceRecord to JSON String
                     .startupOptions(StartupOptions.initial())
                     .build();
@@ -50,19 +72,20 @@ public class FlinkMysqlApplication {
                     .setBulkFlushBackoffStrategy(FlushBackoffType.EXPONENTIAL, 3, 1000)
                     .build();
 
-            // env
-            //         // 每3000毫秒执行一次checkpoint,需要kafka
-            //         .enableCheckpointing(3000)
-            //         .getCheckpointConfig()
-            //         .setCheckpointStorage("file:///opt/flink/storage");
+            CheckpointConfig checkpointConfig = env.getCheckpointConfig();
+            checkpointConfig.setCheckpointStorage(checkpointStorage);
+            // 保留
+            checkpointConfig.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
 
             env
+                    // 每3000毫秒执行一次checkpoint
+                    .enableCheckpointing(3000)
                     .setParallelism(1)
                     .fromSource(mySqlSource, WatermarkStrategy.noWatermarks(), "MySQL Source")
                     .sinkTo(elasticsearchSink);
                     // .print("==>");
 
-            env.execute("Print MySQL Binlog");
+            env.execute("MySQL Binlog To Es");
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -71,23 +94,28 @@ public class FlinkMysqlApplication {
     private static IndexRequest createIndexRequest(String element) {
         JsonObject jsonObject = JsonParser.parseString(element).getAsJsonObject();
         JsonObject after = jsonObject.getAsJsonObject("after");
-        // id
-        String id = after.get("id").getAsString();
-        // map
-        Type type = new TypeToken<Map<String, Object>>() {
-        }.getType();
 
         Gson gson = new Gson();
-        Map<String, Object> map = gson.fromJson(after, type);
+        Book book = gson.fromJson(after, Book.class);
 
-        // 时间处理，cdc将时间改为了时间戳
-        int publishDate = after.get("publish_date").getAsInt();
-        LocalDate localDate = LocalDate.ofEpochDay(publishDate);
-        map.put("publish_date", localDate);
-
+        // id
+        String id = book.getId().toString();
         return Requests.indexRequest()
                 .index("book")
                 .id(id)
-                .source(map);
+                .source(book.toMap());
+    }
+
+    @Getter
+    @Setter
+    static class Book {
+        private Long id;
+        private String title;
+        private BigDecimal price;
+        private LocalDate publish_date;
+
+        public Map<String, Object> toMap(){
+            return Map.of("id", id.toString(), "title", title, "price", price, "publish_date", publish_date);
+        }
     }
 }
